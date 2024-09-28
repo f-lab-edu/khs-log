87e99b0a1579b1c08d4dc71f418096c014420937
dev-khs <kyo3553@naver.com>
2024-09-28 21:37:10 +0900

    feat: add common styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,27 @@
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        pastelPink: '#f7d3dc',
+        pastelBlue: '#d3e5f7',
+        pastelYellow: '#f7ecd3',
+        pastelPurple: '#e5d3f7',
+        darkGray: '#2f2f2f',
+        lightGray: '#ededed',
+        retroCream: '#faf9f6',
+      },
+      boxShadow: {
+        retro: '4px 4px 0px 0px rgba(0, 0, 0, 0.25)', // 레트로 느낌의 그림자 효과
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
