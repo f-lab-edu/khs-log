250636d218a42aba760c58a53976ac4f2540dc5a
dev-khs <kyo3553@naver.com>
2024-10-01 00:10:32 +0900

    refactor: improve responsive layout style

--- src/components/BlogCard/index.tsx
+++ src/components/BlogCard/index.tsx
@@ -2,14 +2,13 @@ import Image from '@/components/Image'
 
 const BlogCard = () => {
   return (
-    <div className="max-w-[50rem] mx-auto">
+    <div className="w-full max-w-[30rem] mx-auto">
       <div className="pt-6 px-6 pb-16 space-y-4 bg-n-2 rounded-[1.25rem] md:p-5 md:pb-14">
-        <div className="relative max-w-[32.5rem] aspect-[1.6] xl:max-w-full mx-auto">
+        <div className="relative aspect-[1.6] w-full">
           <Image
             className="rounded-xl object-cover"
             src="/images/ogu.webp"
             fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1499px) 50vw, 33.33vw"
             alt="blogContent"
           />
         </div>
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,9 +8,8 @@ interface Props {
 }
 
 const Layout = ({children}: Props) => {
-  const [isSideBarVisible, setIsSideBarVisible] = useState(
-    window.innerWidth >= 1179,
-  )
+  const [isSideBarVisible, setIsSideBarVisible] = useState(false)
+  const [isRightSideBarVisible, setIsRightSideBarVisible] = useState(true)
 
   const handleResize = () => {
     if (window.innerWidth <= 1179) {
@@ -18,13 +17,17 @@ const Layout = ({children}: Props) => {
     } else {
       setIsSideBarVisible(false)
     }
+
+    if (window.innerWidth <= 1024) {
+      setIsRightSideBarVisible(false)
+    } else {
+      setIsRightSideBarVisible(true)
+    }
   }
 
   useEffect(() => {
     handleResize()
-
     window.addEventListener('resize', handleResize)
-
     return () => window.removeEventListener('resize', handleResize)
   }, [])
 
@@ -35,16 +38,21 @@ const Layout = ({children}: Props) => {
           isSideBarVisible ? 'pl-24 md:pl-0' : 'pl-80 xl:pl-24 md:pl-0'
         }`}>
         <LeftSideBar isLeftSideBarVisible={isSideBarVisible} />
-        <div className={`flex py-6 md:py-0 h-screen`}>
+        <div className="flex py-6 md:py-0 h-screen">
           <div
-            className={`relative flex grow max-w-full bg-n-1 rounded-[1.25rem] md:rounded-none dark:bg-n-6 pr-[22.5rem] 2xl:pr-80 lg:pr-0`}>
-            <div
-              className={`relative z-2 grow p-10 space-y-10 overflow-y-auto scroll-smooth scrollbar-none 2xl:p-6 md:p-5`}>
-              {children}
+            className={`relative flex grow max-w-full bg-n-1 rounded-[1.25rem] md:rounded-none dark:bg-n-6 ${
+              !isRightSideBarVisible ? 'pr-0' : 'pr-[22.5rem] 2xl:pr-80 lg:pr-0'
+            }`}>
+            <div className="relative z-2 grow p-10 space-y-10 overflow-y-auto scroll-smooth scrollbar-none 2xl:p-6 md:p-5">
+              <div className="relative flex flex-col grow max-w-full md:pt-18">
+                {children}
+              </div>
             </div>
-            <RightCommentBar
-              className={`${!isSideBarVisible && 'md:translate-x-64 md:before:absolute md:before:z-30 md:before:inset-0'}`}
-            />
+            {isRightSideBarVisible && (
+              <RightCommentBar
+                className={`${!isSideBarVisible && 'md:translate-x-64 md:before:absolute md:before:z-30 md:before:inset-0'}`}
+              />
+            )}
           </div>
         </div>
       </div>
