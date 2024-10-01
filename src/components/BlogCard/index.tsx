79c5987a62452af2de6bf24c6ca5164c443eee53
dev-khs <kyo3553@naver.com>
2024-10-01 17:10:52 +0900

    refactor: modify blog-card style

--- src/components/BlogCard/index.tsx
+++ src/components/BlogCard/index.tsx
@@ -1,10 +1,16 @@
+import Link from 'next/link'
+
 import Image from '@/components/Image'
 
-const BlogCard = () => {
+interface Props {
+  url: string
+}
+
+const BlogCard = ({url}: Props) => {
   return (
-    <div className="max-w-[50rem]">
-      <div className="pt-6 px-6 pb-16 space-y-4 bg-n-2 rounded-[1.25rem] md:p-5 md:pb-14">
-        <div className="relative max-w-[32.5rem] aspect-[1.6] xl:max-w-full">
+    <div className="w-full max-w-[30rem] mx-auto">
+      <div className="pt-6 px-6 pb-6 space-y-4 bg-n-2 rounded-[1.25rem] md:p-5 md:pb-14">
+        <div className="relative aspect-[2.4] w-full">
           <Image
             className="rounded-xl object-cover"
             src="/images/ogu.webp"
@@ -13,11 +19,16 @@ const BlogCard = () => {
             alt="blogContent"
           />
         </div>
-        <div className="mt-4">블로그 게시글 예시입니다.</div>
+        <div className="mt-4 base1">블로그 제목입니다.</div>
+        <div className="mt-4 truncate">
+          블로그 게시글 예시입니다. 블로그 게시글 예시입니다. 블로그 게시글
+          예시입니다. 블로그 게시글 예시입니다. 블로그 게시글 예시입니다. 블로그
+          게시글 예시입니다. 블로그 게시글 예시입니다.
+        </div>
         <div className="flex flex-wrap">
-          <button className="btn-dark btn-small mr-4 mt-4">
-            <span>View More</span>
-          </button>
+          <Link href={url} className="btn-dark btn-small mr-4">
+            View More
+          </Link>
         </div>
       </div>
     </div>
--- src/templates/MainPage/index.tsx
+++ src/templates/MainPage/index.tsx
@@ -10,7 +10,7 @@ const MainPage = () => {
   return (
     <>
       <LeftSideBar />
-      <BlogCard />
+      <BlogCard url="/" />
       <RightCommentBar />
     </>
   )
