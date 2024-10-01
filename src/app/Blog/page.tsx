bd9347d81efaa2accfeccfc8a3dc13e4eccc0a94
dev-khs <kyo3553@naver.com>
2024-10-01 20:11:49 +0900

    feat: add BlogPage

---
+++ src/app/Blog/page.tsx
@@ -0,0 +1,9 @@
+import BlogPage from '@/templates/BlogPage'
+
+import type {NextPage} from 'next'
+
+const Blog: NextPage = () => {
+  return <BlogPage />
+}
+
+export default Blog
---
+++ src/templates/BlogPage/index.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React from 'react'
+
+import BlogCard from '@/components/BlogCard'
+import Layout from '@/components/Layout'
+
+const BlogPage = () => {
+  return (
+    <>
+      <Layout>
+        <BlogCard />
+      </Layout>
+    </>
+  )
+}
+
+export default BlogPage
