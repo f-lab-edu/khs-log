750ee7f50af0eb63e0395a971ac0732250f79d97
dev-khs <kyo3553@naver.com>
2024-10-05 17:24:10 +0900

    feat: add Input component

--- src/components/MarkdownEditor/index.tsx
+++ src/components/MarkdownEditor/index.tsx
@@ -4,9 +4,12 @@ import {useCallback, useEffect, useState} from 'react'
 import {remark} from 'remark'
 import html from 'remark-html'
 
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+
 const MarkdownEditor = () => {
-  const [title, setTitle] = useState<string>('')
-  const [content, setContent] = useState<string>('')
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
   const [htmlContent, setHtmlContent] = useState('')
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -30,30 +33,38 @@ const MarkdownEditor = () => {
 
   return (
     <div className="flex flex-col h-screen p-4">
+      <div className="flex justify-end items-center h-18">
+        <Button
+          name="게시글 등록"
+          onClick={() => console.log('click login button')}
+          className="border border-gray-300"
+        />
+      </div>
       <div className="mb-4">
-        <h2 className="text-xl font-bold mb-2">Title</h2>
-        <input
-          type="text"
+        <h2 className="text-xl font-bold mb-2">제목</h2>
+        <Input
           value={title}
+          placeholder="제목을 입력해주세요."
           onChange={handleTitleChange}
-          className="w-full p-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500"
-          placeholder="Enter your title here..."
+          className={
+            'w-full p-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500'
+          }
         />
       </div>
       <div className="flex-1 flex flex-col">
-        <h2 className="text-xl font-bold mb-2">Preview</h2>
+        <h2 className="text-xl font-bold mb-2">미리보기</h2>
         <div
           className="flex-1 overflow-y-auto p-4 border border-gray-300 rounded-md shadow-sm bg-white"
           dangerouslySetInnerHTML={{__html: htmlContent}}
         />
       </div>
       <div className="mt-4">
-        <h2 className="text-xl font-bold mb-2">Contents</h2>
+        <h2 className="text-xl font-bold mb-2">내용</h2>
         <textarea
           value={content}
           onChange={handleContentChange}
           className="w-full h-48 p-4 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 resize-none"
-          placeholder="Enter your markdown content here..."
+          placeholder="내용을 입력해주세요."
         />
       </div>
     </div>
