17a085ea48feec65cbbad4aabff0a3834f67ba55
dev-khs <166009647+dev-khs@users.noreply.github.com>
2024-10-09 14:28:54 +0900

    feat: Implement BlogPage screen (#14)
    * feat: add LeftSideBar component
    
    * feat: add RightCommentBar
    
    * chore: resolve ci issue
    
    * feat: add BlogCard
    
    * refactor: remove useless style
    
    * feat: add Layout container
    
    * feat: add responsive layout style
    
    * feat: add window event listener
    
    * refactor: improve responsive layout style
    
    * refactor: improve responsive layout style
    
    * feat: add new styles
    
    * feat: add button component
    
    * refactor: modify blog-card style
    
    * feat: add comment box component
    
    * feat: add icon component
    
    * feat: add new icons
    
    * feat: add BlogPage
    
    * refactor: add home icon
    
    * refactor: modify blog-card style
    
    ---------
    
    Co-authored-by: KHS <khs@KHSui-MacBookPro-2.local>

--- src/components/BlogCard/index.tsx
+++ src/components/BlogCard/index.tsx
@@ -1,16 +1,21 @@
 import Link from 'next/link'
+import {usePathname} from 'next/navigation'
 
 import Image from '@/components/Image'
 
 interface Props {
-  url: string
+  url?: string
 }
 
 const BlogCard = ({url}: Props) => {
+  const pathname = usePathname()
+  const isBlogPage = pathname.includes('Blog')
+
   return (
     <div className="w-full max-w-[30rem] mx-auto">
       <div className="pt-6 px-6 pb-6 space-y-4 bg-n-2 rounded-[1.25rem] md:p-5 md:pb-14">
-        <div className="relative aspect-[2.4] w-full">
+        <div
+          className={`relative w-full ${isBlogPage ? 'aspect-[1]' : 'aspect-[2.4]'} `}>
           <Image
             className="rounded-xl object-cover"
             src="/images/ogu.webp"
@@ -19,16 +24,18 @@ const BlogCard = ({url}: Props) => {
           />
         </div>
         <div className="mt-4 base1">블로그 제목입니다.</div>
-        <div className="mt-4 truncate">
+        <div className={`mt-4 ${!isBlogPage && 'truncate'}`}>
           블로그 게시글 예시입니다. 블로그 게시글 예시입니다. 블로그 게시글
           예시입니다. 블로그 게시글 예시입니다. 블로그 게시글 예시입니다. 블로그
           게시글 예시입니다. 블로그 게시글 예시입니다.
         </div>
-        <div className="flex flex-wrap">
-          <Link href={url} className="btn-dark btn-small mr-4">
-            View More
-          </Link>
-        </div>
+        {url && (
+          <div className="flex flex-wrap">
+            <Link href={url} className="btn-dark btn-small mr-4">
+              View More
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   )
