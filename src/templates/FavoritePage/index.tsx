3c75ad5e1ad327de5ab54f7d23222bc8b8c6bf19
dev-khs <kyo3553@naver.com>
2024-10-12 13:40:24 +0900

    feat: implement fetch favorites data

--- src/templates/FavoritePage/index.tsx
+++ src/templates/FavoritePage/index.tsx
@@ -1,11 +1,27 @@
 'use client'
 
-import React from 'react'
+import axios from 'axios'
+import React, {useCallback, useEffect, useState} from 'react'
 
 import BlogList from '@/components/BlogList'
 import Layout from '@/components/Layout'
+import {type Database} from '@/supabase/database.types'
 
 const FavoritePage = () => {
+  const [favoritesData, setFavoritesData] =
+    useState<Database['public']['Tables']['favorites']['Row'][]>()
+
+  const fetchFavoritesData = useCallback(async () => {
+    const res = await axios(`/api/Favorite`)
+    const data = await res.data
+
+    setFavoritesData(data)
+  }, [])
+
+  useEffect(() => {
+    void fetchFavoritesData()
+  }, [fetchFavoritesData])
+
   return (
     <>
       <Layout>
