11dc4c6b146a5ea27574a2683eb057363660bc0c
dev-khs <kyo3553@naver.com>
2024-10-13 12:51:13 +0900

    feat: implement comment delete UI

--- src/components/RightCommentBar/CommentBox.tsx
+++ src/components/RightCommentBar/CommentBox.tsx
@@ -1,37 +1,78 @@
 import Link from 'next/link'
-import {type MouseEvent} from 'react'
+import {useCallback, useState} from 'react'
 
+import Button from '@/components/Button'
+import Icon from '@/components/Icon'
+import TooltipModal from '@/components/TooltipModal'
+import {useUser} from '@/store/user'
 import {type Database} from '@/supabase/database.types'
 
 interface Props {
   isDisabled?: boolean
+  isTooltipVisible?: boolean
   item: Database['public']['Tables']['comments']['Row']
+  onClickPositiveButton?: () => void
+  onClickNegativeButton?: () => void
 }
 
-const CommentBox = ({item, isDisabled = false}: Props) => {
-  const handleClick = (e: MouseEvent<HTMLAnchorElement>) => {
-    if (isDisabled) {
-      e.preventDefault()
-    }
-  }
+const CommentBox = ({
+  item,
+  isDisabled = false,
+  onClickPositiveButton,
+  onClickNegativeButton,
+}: Props) => {
+  const user = useUser(state => state.user)
+
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false)
+
+  const isDeleteButtonVisible = user?.id === item.user_id
+
+  const handlePositiveButton = useCallback(() => {
+    onClickPositiveButton?.()
+    setIsTooltipVisible(false)
+  }, [onClickPositiveButton])
+
+  const handleNegativeButton = useCallback(() => {
+    onClickNegativeButton?.()
+    setIsTooltipVisible(false)
+  }, [onClickNegativeButton])
 
   return (
-    <div className="relative">
-      <Link
-        className="block mt-2"
-        href={isDisabled ? '' : `/BlogDetail/${item.post_id}`}
-        onClick={handleClick}>
-        <div className="group py-3 px-3 rounded-xl transition-colors hover:bg-n-3/75">
-          <div className="truncate base1 font-semibold text-n-6">
-            {item.username}
+    <>
+      <div className="relative flex flex-row justify-between items-center">
+        <Link
+          className={`block mt-2 ${isDisabled && 'cursor-default'}`}
+          href={isDisabled ? '' : `/BlogDetail/${item.post_id}`}
+          onClick={event => isDisabled && event.preventDefault()}>
+          <div
+            className={`group py-3 px-3 rounded-xl transition-colors ${!isDisabled && 'hover:bg-n-3/75'}`}>
+            <div className="truncate base1 font-semibold text-n-6">
+              {item.username}
+            </div>
+            <div className="mt-1 base2 text-n-5">{item.content}</div>
+            <div className="flex justify-between items-center mt-2">
+              <div className="caption2 text-n-4/75">{item.created_at}</div>
+            </div>
           </div>
-          <div className="mt-1 base2 text-n-5">{item.content}</div>
-          <div className="flex justify-between items-center mt-2">
-            <div className="caption2 text-n-4/75">{item.created_at}</div>
-          </div>
-        </div>
-      </Link>
-    </div>
+        </Link>
+        {isDeleteButtonVisible && (
+          <>
+            <div onClick={() => setIsTooltipVisible(!isTooltipVisible)}>
+              <Icon
+                iconName="delete"
+                className="cursor-pointer fill-accent-1"
+              />
+            </div>
+            <TooltipModal
+              isModalVisible={isTooltipVisible}
+              title="댓글을 삭제하시겠습니까?">
+              <Button buttonName="예" onClick={handlePositiveButton} />
+              <Button buttonName="아니오" onClick={handleNegativeButton} />
+            </TooltipModal>
+          </>
+        )}
+      </div>
+    </>
   )
 }
 
--- src/components/RightCommentBar/index.tsx
+++ src/components/RightCommentBar/index.tsx
@@ -1,4 +1,3 @@
-import {usePathname} from 'next/navigation'
 import {useCallback} from 'react'
 import {twMerge} from 'tailwind-merge'
 
@@ -31,10 +30,6 @@ const RightCommentBar = ({
   createComment,
   isBlogDetailPage = true,
 }: Props) => {
-  const pathname = usePathname()
-
-  const isBlogPage = pathname === '/Blog'
-
   const handleInput = useCallback(
     async (content: string) => {
       if (createComment) {
@@ -51,23 +46,27 @@ const RightCommentBar = ({
   return (
     <div
       className={twMerge(
-        `absolute top-0 right-0 bottom-0 flex flex-col w-[22.5rem] pt-[8rem] ${!isBlogPage && 'pb-24'}  bg-n-1 rounded-r-[1.25rem] border-l border-n-3 shadow-[inset_0_1.5rem_3.75rem_rgba(0,0,0,0.1)] 2xl:w-80 lg:rounded-[1.25rem] lg:invisible lg:opacity-0 lg:transition-opacity lg:z-20 lg:border-l-0 lg:shadow-2xl md:fixed md:w-[calc(100%-4rem)] md:border-l md:rounded-none ${className}`,
+        `absolute top-0 right-0 bottom-0 flex flex-col w-[22.5rem] pt-[8rem] ${!isBlogDetailPage && 'pb-24'}  bg-n-1 rounded-r-[1.25rem] border-l border-n-3 shadow-[inset_0_1.5rem_3.75rem_rgba(0,0,0,0.1)] 2xl:w-80 lg:rounded-[1.25rem] lg:invisible lg:opacity-0 lg:transition-opacity lg:z-20 lg:border-l-0 lg:shadow-2xl md:fixed md:w-[calc(100%-4rem)] md:border-l md:rounded-none ${className}`,
       )}>
       <LoginForm className="absolute top-0 left-0 right-0 flex justify-end items-center h-18 px-9 border-b border-n-3 lg:pr-18 md:pr-16" />
       <div className="absolute top-24 left-0 right-0 flex items-center px-9 md:px-6">
-        <div className="base2 text-n-4/75">Comment Length</div>
+        <div className="base2 text-n-4/75">댓글 수</div>
         <div className="ml-3 px-2 bg-n-3 rounded-lg caption1 text-n-4">
           {data?.length}
         </div>
       </div>
       <div className="grow overflow-y-auto scroll-smooth items-center px-6 md:px-3">
-        {data?.map((data, index) => {
+        {data?.map(data => {
           return (
-            <CommentBox key={index} item={data} isDisabled={isBlogDetailPage} />
+            <CommentBox
+              key={`${data.id}`}
+              item={data}
+              isDisabled={isBlogDetailPage}
+            />
           )
         })}
       </div>
-      {!isBlogPage && (
+      {isBlogDetailPage && (
         <div className="absolute left-0 right-0 bottom-0 p-6">
           <CommentInput onClick={handleInput} />
         </div>
