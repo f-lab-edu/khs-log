09987ab474bd00b4010e9c9dc9f22471275a5725
dev-khs <166009647+dev-khs@users.noreply.github.com>
2024-10-20 11:43:19 +0900

    refactor: apply user permission condition (#26)

--- src/components/LeftSideBar/index.tsx
+++ src/components/LeftSideBar/index.tsx
@@ -45,32 +45,38 @@ interface Props {
   isLeftSideBarVisible?: boolean
 }
 
-const LeftSideBar = ({user, isLeftSideBarVisible}: Props) => {
-  const isLogin = user !== null
+const LeftSideBar = ({user, isLeftSideBarVisible = false}: Props) => {
+  const isLogin = !!user
+
+  // 사이드바 클래스
+  const sideBarClass = twMerge(
+    `fixed z-20 top-0 left-0 bottom-0 flex flex-col pt-30 px-4 bg-n-7 transition-opacity md:invisible md:opacity-0`,
+    isLeftSideBarVisible ? 'w-24 pb-38 md:w-16 md:px-0 md:pb-30' : 'w-80 pb-58',
+  )
+
+  // 헤더 클래스
+  const headerClass = twMerge(
+    `absolute top-0 right-0 left-0 flex items-center h-30 pl-7 pr-6`,
+    isLeftSideBarVisible ? 'justify-center md:px-4' : 'justify-between',
+  )
 
   return (
-    <div
-      className={twMerge(
-        `fixed z-20 top-0 left-0 bottom-0 flex flex-col pt-30 px-4 bg-n-7 md:invisible md:opacity-0 md:transition-opacity ${
-          isLeftSideBarVisible
-            ? 'w-24 pb-38 md:w-16 md:px-0 md:pb-30'
-            : 'w-80 pb-58'
-        }`,
-      )}>
-      <div
-        className={`absolute top-0 right-0 left-0 flex items-center h-30 pl-7 pr-6 ${
-          isLeftSideBarVisible ? 'justify-center md:px-4' : 'justify-between'
-        }`}>
+    <div className={sideBarClass}>
+      <div className={headerClass}>
         {!isLeftSideBarVisible && (
           <MainTitle title="khs-log" isLogin={isLogin} />
         )}
       </div>
+
+      {/* Navigation */}
       <div className="grow overflow-y-auto scroll-smooth scrollbar-none">
         <Navigation
           isSideBarVisible={isLeftSideBarVisible}
           items={NAVIGATION}
         />
       </div>
+
+      {/* Profile Section */}
       <div className="absolute left-0 bottom-0 right-0 pb-6 px-4 bg-n-7 before:absolute before:left-0 before:right-0 before:bottom-full before:h-10 before:bg-gradient-to-t before:from-[#131617] before:to-[rgba(19,22,23,0)] before:pointer-events-none md:px-3">
         <Profile
           isSideBarVisible={isLeftSideBarVisible}
