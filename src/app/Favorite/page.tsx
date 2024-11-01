323c401b9421ac773c5e0aa6db8cbea05677603f
dev-khs <166009647+dev-khs@users.noreply.github.com>
2024-11-02 01:28:04 +0900

    chore: remove useless directory

--- src/app/Favorite/page.tsx
+++
@@ -1,9 +0,0 @@
-import FavoritePage from '@/templates/FavoritePage'
-
-import type {NextPage} from 'next'
-
-const Favorite: NextPage = () => {
-  return <FavoritePage />
-}
-
-export default Favorite
