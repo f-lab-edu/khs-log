b65571cb1ecf8ffe3be7d93c023a70f2404fc88b
dev-khs <kyo3553@naver.com>
2024-11-17 23:59:34 +0900

    fix: fix build issue

--- src/app/blogDetail/[id]/page.tsx
+++ src/app/blogDetail/[id]/page.tsx
@@ -2,6 +2,8 @@ import BlogDetailPage from '@/pages/BlogDetailPage'
 
 import type {NextPage} from 'next'
 
+export const runtime = 'edge'
+
 const BlogDetail: NextPage = () => {
   return <BlogDetailPage />
 }
