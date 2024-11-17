4503c72e20e9749584caf401efd1fe335c0da84e
dev-khs <kyo3553@naver.com>
2024-11-18 00:18:08 +0900

    refactor: restore wrong changes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,28 +1,9 @@
-import {getPostsByRange} from '@/app/api/getPosts'
 import BlogPage from '@/pages/BlogPage'
 
-export const runtime = 'edge'
+import type {NextPage} from 'next'
 
-const INITIAL_MAXIMUM_INDEX = 1 // 처음에 불러올 데이터 수
-
-export default async function Blog() {
-  try {
-    const data = await getPostsByRange(0, INITIAL_MAXIMUM_INDEX)
-
-    if (!data || data.length === 0) {
-      return (
-        <div className="text-center text-gray-500">
-          No posts available. Please try again later.
-        </div>
-      )
-    }
-
-    return <BlogPage initialData={data} />
-  } catch (error) {
-    return (
-      <div className="text-center text-red-500">
-        Error loading posts data. Please try again later.
-      </div>
-    )
-  }
+const Blog: NextPage = () => {
+  return <BlogPage />
 }
+
+export default Blog
