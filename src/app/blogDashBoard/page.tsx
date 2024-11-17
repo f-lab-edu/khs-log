a40c840de409365610be529bbf8a31a64305cb1b
dev-khs <kyo3553@naver.com>
2024-11-17 22:32:45 +0900

    refactor: apply server side fetch

--- src/app/blogDashBoard/page.tsx
+++ src/app/blogDashBoard/page.tsx
@@ -1,9 +1,22 @@
+import {getAllPosts} from '@/app/api/getPosts'
 import BlogDashBoardPage from '@/pages/BlogDashBoardPage'
 
-import type {NextPage} from 'next'
+export default async function BlogDashBoard() {
+  try {
+    const data = await getAllPosts()
 
-const BlogDashBoard: NextPage = () => {
-  return <BlogDashBoardPage />
+    if (!data || !data.length) {
+      throw new Error('Posts data is empty or invalid')
+    }
+
+    return <BlogDashBoardPage data={data} />
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error fetching posts data:', error)
+    return (
+      <div className="text-center text-red-500">
+        Error loading posts data. Please try again later.
+      </div>
+    )
+  }
 }
-
-export default BlogDashBoard
--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,3 +1,6 @@
+export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
 export const SKILLS = [
   {name: 'javascript', bg: 'bg-accent-6'},
   {name: 'react', bg: 'bg-accent-7'},
