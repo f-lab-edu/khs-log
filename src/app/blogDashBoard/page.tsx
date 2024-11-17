0e4e5aadd3e821f8d6e10aa6771a4dccc208eca0
dev-khs <kyo3553@naver.com>
2024-11-17 23:15:13 +0900

    fix: fix build issue

--- src/app/blogDashBoard/page.tsx
+++ src/app/blogDashBoard/page.tsx
@@ -1,9 +1,29 @@
-import {getAllPosts} from '@/app/api/getPosts'
 import BlogDashBoardPage from '@/pages/BlogDashBoardPage'
+import {SUPABASE_ANON_KEY, SUPABASE_URL} from '@/shared/constants'
 
 export default async function BlogDashBoard() {
   try {
-    const data = await getAllPosts()
+    if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+      throw new Error(
+        'Supabase URL or ANON KEY is missing in environment variables.',
+      )
+    }
+
+    const response = await fetch(`${SUPABASE_URL}/rest/v1/posts`, {
+      method: 'GET',
+      headers: {
+        apikey: SUPABASE_ANON_KEY,
+        Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-cache',
+    })
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch posts data')
+    }
+
+    const data = await response.json()
 
     if (!data || !data.length) {
       throw new Error('Posts data is empty or invalid')
