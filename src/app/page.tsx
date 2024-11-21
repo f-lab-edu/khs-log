0b8e03b54c29461ec532792fec3ae4380ce3eb39
dev-khs <166009647+dev-khs@users.noreply.github.com>
2024-11-21 23:08:20 +0900

    refactor: improve MainPage (#41)
    * refactor: improve MainPage
    
    * refactor: apply server side fetch
    
    * fix: fix build issue
    
    * fix: fix build issue
    
    * fix: fix build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,41 @@
 import MainPage from '@/pages/MainPage'
+import {SUPABASE_ANON_KEY, SUPABASE_URL} from '@/shared/constants'
 
-const Home = () => {
-  return <MainPage />
+export default async function Home() {
+  try {
+    if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
+      throw new Error(
+        'Supabase URL or ANON KEY is missing in environment variables.',
+      )
+    }
+
+    const response = await fetch(`${SUPABASE_URL}/rest/v1/profile`, {
+      method: 'GET',
+      headers: {
+        apikey: SUPABASE_ANON_KEY,
+        Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+        'Content-Type': 'application/json',
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch profile data')
+    }
+
+    const data = await response.json()
+
+    if (!data || !data.length) {
+      throw new Error('Profile data is empty or invalid')
+    }
+
+    return <MainPage profileData={data[0]} />
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error fetching profile data:', error)
+    return (
+      <div className="text-center text-red-500">
+        Error loading profile data. Please try again later.
+      </div>
+    )
+  }
 }
-
-export default Home
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,105 +1,82 @@
-// MainPage.tsx
 'use client'
 
-import axios from 'axios'
 import Image from 'next/image'
-import React, {useCallback, useEffect, useState} from 'react'
+import React from 'react'
 
 import LoginForm from '@/features/auth/components/LoginForm'
-import MainPageSkeleton from '@/pages/MainPage/ui/MainPageSkeleton'
 import IconRow, {type IconsType} from '@/shared/components/IconRow'
 import Typography from '@/shared/components/Typography'
 import {type ProfileData} from '@/shared/types'
 import Layout from '@/widgets/Layout/components'
 
 interface Props {
-  profileData: ProfileData | null
-  isLoading: boolean
+  profileData: ProfileData
 }
 
-const Page = ({profileData, isLoading}: Props) => {
+const Page = ({profileData}: Props) => {
+  const {
+    mainTitle = 'Default Title',
+    subTitle = 'Default Subtitle',
+    contents = 'Default Contents',
+    skills = [],
+    tools = [],
+    imageUrl = '',
+  } = profileData
+
   return (
-    <div>
-      <Layout isMainView>
-        <LoginForm className="flex justify-end items-center h-10 mb-6 selection:border-b" />
-        {isLoading ? (
-          <MainPageSkeleton />
-        ) : (
-          <>
-            <Typography
-              text={profileData?.mainTitle ?? ''}
-              className="mb-4 h2 md:h3 font-black"
+    <Layout isMainView>
+      <LoginForm className="flex justify-end items-center h-10 mb-6 selection:border-b" />
+      <Typography text={mainTitle} className="mb-4 h2 md:h3 font-black" />
+      <Typography text={subTitle} className="mb-12 body1 text-n-4 md:mb-6" />
+      <div className="flex py-8 border-t lg:block md:py-8 dark:border-n-5">
+        <div className="shrink-0 w-[21rem] lg:w-full lg:mb-10 lg:pr-0">
+          <Typography
+            text={contents}
+            className="mt-4 base2 text-n-4 whitespace-pre-line"
+          />
+          <div className="lg:flex lg:justify-around md:flex-col md:justify-normal">
+            {skills && (
+              <IconRow
+                icons={skills as unknown as IconsType}
+                title="Skills"
+                className="min-w-[160px]"
+              />
+            )}
+            {tools && (
+              <IconRow
+                icons={tools as unknown as IconsType}
+                title="Tools"
+                className="min-w-[160px]"
+              />
+            )}
+          </div>
+        </div>
+        {imageUrl && (
+          <div className="grow px-5 justify-center">
+            <Image
+              className="w-full max-h-[500px] object-cover rounded-3xl md:rounded-xl"
+              src={imageUrl}
+              alt="avatar"
+              width={580}
+              height={500}
             />
-            <Typography
-              text={profileData?.subTitle ?? ''}
-              className="mb-12 body1 text-n-4 md:mb-6"
-            />
-            <div className="flex py-8 border-t lg:block md:py-8 dark:border-n-5">
-              <div className="shrink-0 w-[21rem] lg:w-full lg:mb-10 lg:pr-0">
-                <Typography
-                  text={profileData?.contents ?? ''}
-                  className="mt-4 base2 text-n-4 whitespace-pre-line"
-                />
-                <div className="lg:flex lg:justify-around md:flex-col md:justify-normal">
-                  {profileData?.skills && (
-                    <IconRow
-                      icons={profileData.skills as unknown as IconsType}
-                      title="Skills"
-                      className="min-w-[160px]"
-                    />
-                  )}
-                  {profileData?.tools && (
-                    <IconRow
-                      icons={profileData?.tools as unknown as IconsType}
-                      title="Tools"
-                      className="min-w-[160px]"
-                    />
-                  )}
-                </div>
-              </div>
-              {profileData?.imageUrl && (
-                <div className="grow px-5 justify-center">
-                  <Image
-                    className="w-full max-h-[500px] object-cover rounded-3xl md:rounded-xl"
-                    src={profileData.imageUrl}
-                    alt="avatar"
-                    width={580}
-                    height={500}
-                  />
-                </div>
-              )}
-            </div>
-          </>
+          </div>
         )}
-      </Layout>
-    </div>
+      </div>
+    </Layout>
   )
 }
 
-const MainPage = () => {
-  const [profileData, setProfileData] = useState<ProfileData | null>(null)
-  const [isLoading, setIsLoading] = useState(true)
+const MainPage = ({profileData}: Props) => {
+  if (!profileData) {
+    return (
+      <div className="text-center text-red-500">
+        Error: Profile data is unavailable. Please try again later.
+      </div>
+    )
+  }
 
-  const fetchProfileData = useCallback(async () => {
-    try {
-      const {data} = await axios.get<{profileData: ProfileData[]}>('/api/main')
-
-      if (data.profileData[0]) {
-        setProfileData(data.profileData[0])
-      }
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error('Error fetching profile data:', error)
-    } finally {
-      setIsLoading(false)
-    }
-  }, [])
-
-  useEffect(() => {
-    fetchProfileData()
-  }, [fetchProfileData])
-
-  return <Page isLoading={isLoading} profileData={profileData} />
+  return <Page profileData={profileData} />
 }
 
 export default MainPage
